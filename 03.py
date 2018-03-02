834623d2a5a423c2c17dd8d4acad13a22c3f240e
Eric Matt <erichmatt@gmail.com>
2018-03-02 16:13:25 -0500

    added files

---
+++ 03.py
@@ -0,0 +1,7 @@
+import pyrosim
+sim = pyrosim.Simulator( play_paused = True , eval_time=1000)
+whiteObject = sim.send_cylinder( x=0 , y=.5, z=1.1 , length = 1.0 , radius=0.1 , r1 = 0, r2 = 1 , r3 = 0)
+redObject = sim.send_cylinder( x=0 , y=0 , z=0.6 , length = 1.0 , radius=0.1 , r=1 , g=0 , b=0)
+joint = sim.send_hinge_joint( first_body_id = whiteObject , second_body_id = redObject , x = 0 , y = 0, z = 1 , n1 = 0 , n2 = 0 , n3 = 1 )
+ 
+sim.start()
