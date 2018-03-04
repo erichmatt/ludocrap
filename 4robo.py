1a3aa3f200ed8bcc9436b7bc415395de7dea955a
Cyrus B <cyrusb252@gmail.com>
2018-03-04 11:54:32 -0500

    I did a thing. developing synapses.

--- 4robo.py
+++ 4robo.py
@@ -2,8 +2,9 @@ import sys
 sys.path.insert(0, '../..')
 
 import pyrosim
+import math
 
-sim = pyrosim.Simulator(play_paused=True,debug=True,eval_time=2000)
+sim = pyrosim.Simulator(play_paused=True,debug=True,eval_time=5000)
 
 body = sim.send_box(x=0,y=0,z=0.5,length=0.5,width=0.5,height=0.1)
 leg1 = sim.send_cylinder(x=0,y=-0.5,z=0.5, r=0,g=1,b=0,length=0.5,
@@ -39,9 +40,24 @@ mneuron2=sim.send_motor_neuron(joint2)
 mneuron3=sim.send_motor_neuron(joint3)
 mneuron4=sim.send_motor_neuron(joint4)
 
-sim.send_synapse(sneuron1,mneuron1,weight=-0.5)
-sim.send_synapse(sneuron2,mneuron2,weight=-0.5)
-sim.send_synapse(sneuron3,mneuron3,weight=-0.5)
-sim.send_synapse(sneuron4,mneuron4,weight=-0.5)
+fneuron1=sim.send_function_neuron(math.sin)
+fneuron2=sim.send_function_neuron(math.sin)
+fneuron3=sim.send_function_neuron(math.sin)
+fneuron4=sim.send_function_neuron(math.sin)
+
+#sim.send_synapse(sneuron1,mneuron1,weight=-10)
+#sim.send_synapse(sneuron2,mneuron2,weight=0.5)
+#sim.send_synapse(sneuron3,mneuron3,weight=-10)
+#sim.send_synapse(sneuron4,mneuron4,weight=0.5)
+
+sim.send_developing_synapse(fneuron1,mneuron1,start_weight=10,end_weight=10,
+                            start_time=0, end_time=1)
+sim.send_developing_synapse(fneuron2,mneuron2,start_weight=-10,end_weight=10,
+                            start_time=0, end_time=1)
+sim.send_developing_synapse(fneuron3,mneuron3,start_weight=10,end_weight=-10,
+                            start_time=0, end_time=1)
+sim.send_developing_synapse(fneuron4,mneuron4,start_weight=10,end_weight=10,
+                            start_time=0, end_time=1)
+
 
 sim.start()
