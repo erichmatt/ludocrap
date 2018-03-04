c68ea39a3b393643d0edf5394d5989fa7f650755
Eric <erichmatt@gmail.com>
2018-03-04 09:00:56 -0500

    fixed the joint positions

--- 4robo.py
+++ 4robo.py
@@ -16,13 +16,13 @@ leg4 = sim.send_cylinder(x=-0.5,y=0,z=0.5, r=1,g=0,b=1,length=0.5,
                          r1=1,r2=0,r3=0,radius=0.1)
 
 joint1 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg1,
-                              x=0,y=0.25,z=0.5,n1=1,n2=0,n3=0)
-joint2 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg2,
                               x=0,y=-0.25,z=0.5,n1=1,n2=0,n3=0)
+joint2 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg2,
+                              x=0,y=0.25,z=0.5,n1=1,n2=0,n3=0)
 joint3 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg3,
                               x=0.25,y=0,z=0.5,n1=0,n2=1,n3=0)
 joint4 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg4,
-                              x=-0.25,y=0,z=0.5,n1=0,n2=1,n3=0)
+                              x=-.250,y=0,z=0.5,n1=0,n2=1,n3=0)
 
 sensor1=sim.send_touch_sensor(body_id=leg1)
 sensor2=sim.send_touch_sensor(body_id=leg2)
