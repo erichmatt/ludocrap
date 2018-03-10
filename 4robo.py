3989418ca525f9e873b99e3150f7a9cf4c076c60
Eric Matt <erichmatt@gmail.com>
2018-03-10 10:03:41 -0500

    made our shins point down

--- 4robo.py
+++ 4robo.py
@@ -6,40 +6,43 @@ import math
 
 sim = pyrosim.Simulator(play_paused=True,debug=True,eval_time=3000)
 
-body = sim.send_box(x=0,y=0,z=0.5,length=0.5,width=0.5,height=0.1)
+body = sim.send_box(x=0,y=0,z=1,length=0.5,width=0.5,height=0.1)
 #green leg
-leg1 = sim.send_cylinder(x=0,y=-0.5,z=0.5, r=0,g=1,b=0,length=0.5,
+leg1 = sim.send_cylinder(x=0,y=-0.5,z=1, r=0,g=1,b=0,length=0.5,
                          r1=0,r2=1,r3=0,radius=0.1,)
 #light blue leg
-leg2 = sim.send_cylinder(x=0,y=0.5,z=0.5, r=0,g=1,b=1,length=0.5,
+leg2 = sim.send_cylinder(x=0,y=0.5,z=1, r=0,g=1,b=1,length=0.5,
                          r1=0,r2=1,r3=0,radius=0.1)
 #dark blue leg
-leg3 = sim.send_cylinder(x=0.5,y=0,z=0.5, r=0,g=0,b=1,length=0.5,
+leg3 = sim.send_cylinder(x=0.5,y=0,z=1, r=0,g=0,b=1,length=0.5,
                          r1=1,r2=0,r3=0,radius=0.1)
 #purple leg
-leg4 = sim.send_cylinder(x=-0.5,y=0,z=0.5, r=1,g=0,b=1,length=0.5,
+leg4 = sim.send_cylinder(x=-0.5,y=0,z=1, r=1,g=0,b=1,length=0.5,
                          r1=1,r2=0,r3=0,radius=0.1)
-shin1 = sim.send_cylinder(x=1,y=0,z=0.5,length=0.5,radius=0.1,r1=1,r2=0,r3=0)
-shin2 = sim.send_cylinder(x=-1,y=0,z=0.5,length=0.5,radius=0.1,r1=1,r2=0,r3=0)
-shin3 = sim.send_cylinder(x=0,y=1,z=0.5,length=0.5,radius=0.1,r1=0,r2=1,r3=0)
-shin4 = sim.send_cylinder(x=0,y=-1,z=0.5,length=0.5,radius=0.1,r1=0,r2=1,r3=0)
+shin1 = sim.send_cylinder(x=0,y=-0.75,z=0.75,length=0.5,radius=0.1,r1=0,r2=0,r3=1)
+shin2 = sim.send_cylinder(x=0,y=.75,z=0.75,length=0.5,radius=0.1,r1=0,r2=0,r3=1)
+shin3 = sim.send_cylinder(x=.75,y=0,z=0.75,length=0.5,radius=0.1,r1=0,r2=0,r3=1)
+shin4 = sim.send_cylinder(x=-.75,y=0,z=0.75,length=0.5,radius=0.1,r1=0,r2=0,r3=1)
+
+
 
 joint1 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg1,
-                              x=0,y=-0.25,z=0.5,n1=1,n2=0,n3=0)
+                              x=0,y=-0.25,z=1,n1=1,n2=0,n3=0)
 joint2 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg2,
-                              x=0,y=0.25,z=0.5,n1=1,n2=0,n3=0)
+                              x=0,y=0.25,z=1,n1=1,n2=0,n3=0)
 joint3 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg3,
-                              x=0.25,y=0,z=0.5,n1=0,n2=1,n3=0)
+                              x=0.25,y=0,z=1,n1=0,n2=1,n3=0)
 joint4 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg4,
-                              x=-.25,y=0,z=0.5,n1=0,n2=1,n3=0)
-joint5 = sim.send_hinge_joint(first_body_id=leg3,second_body_id=shin1,
-                              x=0.75,y=0,z=0.5,n1=0,n2=1,n3=0)
-joint6 = sim.send_hinge_joint(first_body_id=leg4,second_body_id=shin2,
-                              x=-0.75,y=0,z=0.5,n1=0,n2=1,n3=0)
-joint7 = sim.send_hinge_joint(first_body_id=leg2,second_body_id=shin3,
-                              x=0,y=0.75,z=0.5,n1=1,n2=0,n3=0)
-joint8 = sim.send_hinge_joint(first_body_id=leg1,second_body_id=shin4,
-                              x=0,y=-0.75,z=0.5,n1=1,n2=0,n3=0)
+                              x=-.25,y=0,z=1,n1=0,n2=1,n3=0)
+
+joint5 = sim.send_hinge_joint(first_body_id=leg3,second_body_id=shin3,
+                              x=0.75,y=0,z=1,n1=0,n2=1,n3=0)
+joint6 = sim.send_hinge_joint(first_body_id=leg4,second_body_id=shin4,
+                              x=-0.75,y=0,z=1,n1=0,n2=1,n3=0)
+joint7 = sim.send_hinge_joint(first_body_id=leg2,second_body_id=shin2,
+                              x=0,y=0.75,z=1,n1=1,n2=0,n3=0)
+joint8 = sim.send_hinge_joint(first_body_id=leg1,second_body_id=shin1,
+                              x=0,y=-0.75,z=1,n1=1,n2=0,n3=0)
 
 sensor1=sim.send_touch_sensor(body_id=shin1)
 sensor2=sim.send_touch_sensor(body_id=shin2)
