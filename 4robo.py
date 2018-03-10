feaed1f0b400d0747a85a755252bf2fbabd64a1e
Eric Matt <erichmatt@gmail.com>
2018-03-10 10:12:33 -0500

    it kinda works

--- 4robo.py
+++ 4robo.py
@@ -26,23 +26,25 @@ shin4 = sim.send_cylinder(x=-.75,y=0,z=0.75,length=0.5,radius=0.1,r1=0,r2=0,r3=1
 
 
 
-joint1 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg1,
+joint1a = sim.send_hinge_joint(first_body_id=body,second_body_id=leg1,
                               x=0,y=-0.25,z=1,n1=1,n2=0,n3=0)
-joint2 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg2,
+joint2a = sim.send_hinge_joint(first_body_id=body,second_body_id=leg2,
                               x=0,y=0.25,z=1,n1=1,n2=0,n3=0)
-joint3 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg3,
+joint3a = sim.send_hinge_joint(first_body_id=body,second_body_id=leg3,
                               x=0.25,y=0,z=1,n1=0,n2=1,n3=0)
-joint4 = sim.send_hinge_joint(first_body_id=body,second_body_id=leg4,
+joint4a = sim.send_hinge_joint(first_body_id=body,second_body_id=leg4,
                               x=-.25,y=0,z=1,n1=0,n2=1,n3=0)
 
-joint5 = sim.send_hinge_joint(first_body_id=leg3,second_body_id=shin3,
-                              x=0.75,y=0,z=1,n1=0,n2=1,n3=0)
-joint6 = sim.send_hinge_joint(first_body_id=leg4,second_body_id=shin4,
-                              x=-0.75,y=0,z=1,n1=0,n2=1,n3=0)
-joint7 = sim.send_hinge_joint(first_body_id=leg2,second_body_id=shin2,
-                              x=0,y=0.75,z=1,n1=1,n2=0,n3=0)
-joint8 = sim.send_hinge_joint(first_body_id=leg1,second_body_id=shin1,
+joint1b = sim.send_hinge_joint(first_body_id=leg1,second_body_id=shin1,
                               x=0,y=-0.75,z=1,n1=1,n2=0,n3=0)
+joint2b = sim.send_hinge_joint(first_body_id=leg2,second_body_id=shin2,
+                              x=0,y=0.75,z=1,n1=1,n2=0,n3=0)
+joint3b = sim.send_hinge_joint(first_body_id=leg3,second_body_id=shin3,
+                              x=0.75,y=0,z=1,n1=0,n2=1,n3=0)
+joint4b = sim.send_hinge_joint(first_body_id=leg4,second_body_id=shin4,
+                              x=-0.75,y=0,z=1,n1=0,n2=1,n3=0)
+
+
 
 sensor1=sim.send_touch_sensor(body_id=shin1)
 sensor2=sim.send_touch_sensor(body_id=shin2)
@@ -54,10 +56,10 @@ sneuron2=sim.send_sensor_neuron(sensor2)
 sneuron3=sim.send_sensor_neuron(sensor3)
 sneuron4=sim.send_sensor_neuron(sensor4)
 
-mneuron1=sim.send_motor_neuron(joint5)
-mneuron2=sim.send_motor_neuron(joint6)
-mneuron3=sim.send_motor_neuron(joint7)
-mneuron4=sim.send_motor_neuron(joint8)
+mneuron1=sim.send_motor_neuron(joint1a)
+mneuron2=sim.send_motor_neuron(joint2a)
+mneuron3=sim.send_motor_neuron(joint3a)
+mneuron4=sim.send_motor_neuron(joint4a)
 
 fneuron1=sim.send_function_neuron(math.sin)
 fneuron2=sim.send_function_neuron(math.sin)
